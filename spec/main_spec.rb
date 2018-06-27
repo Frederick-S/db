78c120447fd4b795312148c02447d2855dfe0513
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-27 17:31:43 +0800

    Update test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -131,6 +131,25 @@ describe "database" do
         ])
     end
 
+    it "prints an error message if there is a duplicate id" do
+        script = [
+            "insert 1 user1 person1@example.com",
+            "insert 1 user1 person1@example.com",
+            "select",
+            ".exit"
+        ]
+
+        result = run_script(script)
+
+        expect(result).to match_array([
+            "db > Executed.",
+            "db > Error: Duplicate key.",
+            "db > (1, user1, person1@example.com)",
+            "Executed.",
+            "db > "
+        ])
+    end
+
     it "allows printing out the structure of a one-node btree" do
         script = (1..3).map do |i|
             "insert #{i} user#{i} person#{i}@example.com"
@@ -150,45 +169,6 @@ describe "database" do
             " - 1",
             " - 2",
             " - 3",
-            "db > "
-        ])
-    end
-
-    it "prints constants" do
-        script = [
-            ".constants",
-            ".exit"
-        ]
-
-        result = run_script(script)
-
-        expect(result).to match_array([
-            "db > Constants:",
-            "ROW_SIZE: 293",
-            "COMMON_NODE_HEADER_SIZE: 6",
-            "LEAF_NODE_HEADER_SIZE: 10",
-            "LEAF_NODE_CELL_SIZE: 297",
-            "LEAF_NODE_SPACE_FOR_CELLS: 4086",
-            "LEAF_NODE_MAX_CELLS: 13",
-            "db > "
-        ])
-    end
-
-    it "prints an error message if there is a duplicate id" do
-        script = [
-            "insert 1 user1 person1@example.com",
-            "insert 1 user1 person1@example.com",
-            "select",
-            ".exit"
-        ]
-
-        result = run_script(script)
-
-        expect(result).to match_array([
-            "db > Executed.",
-            "db > Error: Duplicate key.",
-            "db > (1, user1, person1@example.com)",
-            "Executed.",
             "db > "
         ])
     end
@@ -225,6 +205,26 @@ describe "database" do
             "  - 13",
             "  - 14",
             "db > Executed.",
+            "db > "
+        ])
+    end
+
+    it "prints constants" do
+        script = [
+            ".constants",
+            ".exit"
+        ]
+
+        result = run_script(script)
+
+        expect(result).to match_array([
+            "db > Constants:",
+            "ROW_SIZE: 293",
+            "COMMON_NODE_HEADER_SIZE: 6",
+            "LEAF_NODE_HEADER_SIZE: 10",
+            "LEAF_NODE_CELL_SIZE: 297",
+            "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+            "LEAF_NODE_MAX_CELLS: 13",
             "db > "
         ])
     end
